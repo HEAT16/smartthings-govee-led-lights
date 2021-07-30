3ca56bfd052b07a01dccc1ebc1adf1ba53152591
Rob <he16at@gmail.com>
2021-07-30 13:11:48 -0400

    Update govee-device-handler.groovy

--- devicetypes/govee-device-handler.groovy
+++ devicetypes/govee-device-handler.groovy
@@ -12,7 +12,7 @@
 metadata {
 	definition (
 		name: "Govee LED Strips (DBDEV)",
-		namespace: "dbdevelopment",
+		namespace: "HEAT16",
 		author: "Dean Berman",
 		description: "Govee LED Strips Integration",
 		category: "My Apps",
